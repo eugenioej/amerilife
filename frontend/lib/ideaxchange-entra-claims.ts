5710d58f6095b50ebfe693c521eec81bc6f6303c
Mccormack <cmccormack@amerilife.com>
2026-07-30 08:37:53 -0400

    Add additional log

--- frontend/lib/ideaxchange-entra-claims.ts
+++ frontend/lib/ideaxchange-entra-claims.ts
@@ -41,6 +41,8 @@ function mergeClaimRecords(
       ? decodeJwtPayload(account.id_token)
       : {};
 
+  console.info("[Entra Raw ID Token Payload]", fromIdToken);
+
   return { ...fromIdToken, ...fromProfile };
 }
 
