d010c8e27f28c391dce43a10fc12b0de267db6db
Viviano <aviviano@amerilife.com>
2026-08-26 12:24:14 -0400

    mastermindsedits

--- frontend/app/(site)/masterminds/a-ri/page.tsx
+++ frontend/app/(site)/masterminds/a-ri/page.tsx
@@ -21,7 +21,7 @@ amerilife:
 
 };
 ======================================== */
-
+/*
 const AgendaDays = [
   {
     title: "Tuesday, September 15, 2026",
@@ -150,6 +150,138 @@ const AgendaDays = [
       },
     ],
   },
+];*/
+
+const AgendaDays = [
+  {
+    title: "Tuesday, September 15, 2026",
+    items: [
+      {
+        text: "Arrivals - please utilize Ubers/Lyft",
+      },
+      {
+        text: "1:30 PM – 4:00 PM: General Session, St. Petersburg Ballroom I & II",
+        children: [
+          {
+            text: "1:30 PM – 2:00 PM: Welcome",
+            children: [
+              {
+                text:
+                  "Speakers: Brian Peterson, Brian Kunkel, and Steve Cooney",
+              },
+            ],
+          },
+          {
+            text: "2:00 PM – 4:00 PM: Best Practices Roundtable",
+          },
+        ],
+      },
+      {
+        text:
+          "6:00 PM – 9:00 PM: Group Dinner at Forbici Modern Italian (approximately a 10-minute walk from the hotel).",
+      },
+    ],
+  },
+
+  {
+    title: "Wednesday, September 16, 2026",
+    items: [
+      {
+        text:
+          "8:00 AM – 8:30 AM: Group Breakfast, St. Petersburg Ballroom III",
+      },
+      {
+        text:
+          "8:30 AM – 12:00 PM: General Session, St. Petersburg Ballroom I & II",
+        children: [
+          {
+            text: "8:30 AM – 8:45 AM: Day 1 Recap",
+            children: [
+              {
+                text: "Speaker: Brian Peterson",
+              },
+            ],
+          },
+          {
+            text:
+              "8:45 AM – 9:45 AM: Fueling Financial Professional Growth Panel",
+          },
+          {
+            text: "9:45 AM – 10:00 AM: Break",
+          },
+          {
+            text: "10:00 AM – 12:00 PM: Wholesaler Showdown",
+            children: [
+              {
+                text: "Speaker: Brian Kunkel",
+              },
+            ],
+          },
+        ],
+      },
+      {
+        text:
+          "12:00 PM – 12:45 PM: Group Lunch, St. Petersburg Ballroom III",
+      },
+      {
+        text:
+          "12:45 PM – 4:00 PM: General Session, St. Petersburg Ballroom I & II",
+        children: [
+          {
+            text: "12:45 PM – 2:15 PM: Carrier Speed Dating",
+          },
+          {
+            text: "2:15 PM – 2:30 PM: Break",
+          },
+          {
+            text: "2:30 PM – 4:00 PM: Carrier Speed Dating",
+          },
+        ],
+      },
+      {
+        text:
+          "6:00 PM – 9:00 PM: Group Dinner at Juno and the Peacock (approximately a 15-minute walk from the hotel).",
+      },
+    ],
+  },
+
+  {
+    title: "Thursday, September 17, 2026",
+    items: [
+      {
+        text:
+          "8:00 AM – 9:00 AM: Group Breakfast, St. Petersburg Ballroom III",
+      },
+      {
+        text:
+          "9:00 AM – 11:00 AM: General Session, St. Petersburg Ballroom I & II",
+        children: [
+          {
+            text: "9:00 AM – 11:00 AM: Elite Wholesaler Prospecting",
+            children: [
+              {
+                text: "Speaker: Sarano Kelley",
+              },
+            ],
+          },
+          {
+            text: "11:00 AM: Wrap-Ups",
+            children: [
+              {
+                text: "Speaker: Brian Peterson",
+              },
+            ],
+          },
+        ],
+      },
+      {
+        text: "11:00 AM: Grab 'n' Go Lunches",
+      },
+      {
+        text: "Departures – please utilize Ubers/Lyft",
+      },
+    ],
+  },
 ];
 
 /* ========================================
@@ -160,7 +292,7 @@ export default function Page() {
     <div className="masterminds-page bg-[#091229]">
       <MastermindsHeader 
         eventtype="A&RI Agenda" 
-        note="Please note the agenda is subject to change. Attire is business casual for meetings and resort casual for group dinners. Unless otherwise noted, group meals combine Health and A&RI distributions."/>
+        note="Please note the agenda is subject to change. Attire is business casual for meetings and resort casual for group dinners."/>
       <MastermindsAgenda 
         agendaDays={AgendaDays}/>
       <MastermindsFooter/>
