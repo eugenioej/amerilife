5d41f95b20aad73f8b0449ccec19d52decdf6168
Mccormack <cmccormack@amerilife.com>
2026-09-14 09:21:11 -0400

    Change title on carrier spotlight page

--- frontend/app/components/ideaxchange/carrier/CarrierSpotlightPage.tsx
+++ frontend/app/components/ideaxchange/carrier/CarrierSpotlightPage.tsx
@@ -13,7 +13,7 @@ export function CarrierSpotlightPage({ carriers }: Props) {
 
   return (
     <div className="bg-white pb-16 md:pb-20">
-      <IdeaXchangePillarBanner title="Career Spotlight" />
+      <IdeaXchangePillarBanner title="Medicare Supplement Carrier Spotlight" />
       <CarrierSpotlightHeroGrid carriers={hero} />
 
       <div className="mx-auto max-w-[var(--container-max)] px-[var(--container-padding-x)] py-10 md:py-14">
